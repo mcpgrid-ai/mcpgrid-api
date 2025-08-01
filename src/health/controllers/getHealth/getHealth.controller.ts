633586e32da4f494e14d1860cb9552aea391e620
Andrii Maiskyi <andrey@wearehelful.com>
2025-08-01 12:47:36 +0100

    feat: search. #1

--- src/health/controllers/getHealth/getHealth.controller.ts
+++ src/health/controllers/getHealth/getHealth.controller.ts
@@ -13,8 +13,7 @@ export class GetHealthController {
       status: 'ok',
       timestamp: new Date().toISOString(),
       uptime: process.uptime(),
-      key: process.env.MEILISEARCH_API_KEY,
-      host: process.env.MEILISEARCH_HOST,
+      cors: process.env.CORS_ORIGIN_WHITELIST,
     };
   }
 }
